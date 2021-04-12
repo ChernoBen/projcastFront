56cf8db4181dd357104f9adea2f6571c8a920481
benjamim <bneto@computacao.cua.ufmt.br>
2021-04-11 21:55:16 -0300

    new init

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,14 @@
 <template>
   <div id="app">
-    <div class="column is-half is-offset-one-quarter">
-      <img src="./assets/pokemon.png" >
-      <hr>
-      <h4 class="is size-7">Pokedex</h4><br>
-      <input class="input is-rounded" type="text" placeholder="Buscar Pokemon" v-model="busca">
-      <button class="button is-medium is-halfwidth is-success" id="buscaBtn" @click="buscarPoke">Buscar</button>  
-       <dir v-for="(poke,index) in filteredPokemons" :key="poke.url">
-      <Pokemon :name="poke.name" :url="poke.url" :num="index"/>
-    </dir>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link :to="{name:'benj',params:{dado:'Francisco'}}">Cadastro</router-link> |
+      <router-link to="/ben">Ben</router-link> 
     </div>
-   
-    
+    <router-view/>
   </div>
 </template>
-
-<script>
-import axios from 'axios'
-import Pokemon from './components/Pokemon'
-
-let url = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0"
-
-export default {
-  name: 'App',
-  data(){
-    return {
-      pokemons:[],
-      filteredPokemons:[],
-      busca:""
-    }
-  },
-  created:function(){
-    
-    axios.get(url).then(res=>{
-      
-      this.pokemons = res.data.results
-      this.filteredPokemons = res.data.results
-    })
-  },
-  components:{
-    Pokemon
-  },
-  methods:{
-
-    buscarPoke:function(){
-
-      this.filteredPokemons = this.pokemons
-      if(this.busca =="" || this.busca == " "){
-
-        this.filteredPokemons =  this.pokemons
-
-      }else {
-
-        this.filteredPokemons =  this.pokemons.filter(pokemons=>pokemons.name == this.busca)
-      }
-
-    }
-  },
-  computed:{
-    // resultado:function(){
-    //   if(this.busca =="" || this.busca == " "){
-    //     return this.pokemons
-    //   }else {
-    //     return this.pokemons.filter(pokemons=>pokemons.name == this.busca)
-    //   }
-    // }
-  }
-
-}
-</script>
 
 <style>
 #app {
@@ -77,9 +17,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-#buscaBtn{
-  margin-top:2%;
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Benj.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type="text" name="email" id="email" placeholder="Email"><br>
+        <input type="text" name="nome" id="nome" placeholder="Nome"><br>
+        <button>Teste!</button>
+        <h2>{{parametroDaRota}}</h2>
+        <router-view/>
+    </div>
+</template>
+<script>
+
+
+export default ({
+    created(){
+        
+    },
+    data(){
+        return{
+            parametroDaRota:"Nada"
+        }
+    }
+    
+})
+</script>
